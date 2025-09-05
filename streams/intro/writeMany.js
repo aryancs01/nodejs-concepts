d0b994aa1b97516e49871f2c1a227d0363cc2cc3
aryancs01 <aryansaxenacs@gmail.com>
2025-09-05 18:19:24 +0530

    streams memory fix

--- streams/intro/writeMany.js
+++ streams/intro/writeMany.js
@@ -18,7 +18,7 @@
 // Callback fn
 // As docs says, it is more useful for expensive operation
 // Execution are not in order
-// Memory ususge is high 
+// Memory ususge is high
 // const fs = require("node:fs");
 // (async () => {
 //   console.time("writeMany");
@@ -31,23 +31,96 @@
 // })();
 // And Actually, it is very fast
 
-
-
 //------------------------------------------------
+
 // Using Streams
 // Its way way faster!
 // Memory usuage is still very high
+// const fs = require("node:fs/promises");
+// (async () => {
+//   console.time("writeMany")
+//   const fileHandle = await fs.open("test.txt","w");
+
+//   const stream = fileHandle.createWriteStream()
+
+//   for(let i=0;i<1000000;i++){
+//     const buff = Buffer.from(` ${i} `,"utf-8")
+//     stream.write(buff)
+//   }
+
+//   console.timeEnd("writeMany")
+// })();
+
+// -----------------------------------------------------------------------
+
+// Streams - Concepts & Memory issue fix
+// const fs = require("node:fs/promises");
+// (async () => {
+//   console.time("writeMany")
+//   const fileHandle = await fs.open("test.txt","w");
+
+//   const stream = fileHandle.createWriteStream()
+
+//   console.log(stream.writableHighWaterMark)
+
+//   const buff = Buffer.alloc(65535, 10)
+//   console.log(stream.write(buff))
+//   console.log(stream.write(Buffer.alloc(1,"a")))
+
+//   console.log(stream.writableLength)
+
+//   stream.on("drain", ()=>{
+//     console.log(stream.writableLength)
+//     console.log("we are now safe to write")
+//   })
+
+//   // for(let i=0;i<1000000;i++){
+//   //   const buff = Buffer.from(` ${i} `,"utf-8")
+//   //   stream.write(buff)
+//   // }
+
+//   console.timeEnd("writeMany")
+// })();
+
+//----------------------------------------------------------------
+
+// Streams -  Memory Fix
 const fs = require("node:fs/promises");
 (async () => {
-  console.time("writeMany")
-  const fileHandle = await fs.open("test.txt","w");
+  console.time("writeMany");
+  const fileHandle = await fs.open("test.txt", "w");
+  const stream = fileHandle.createWriteStream();
 
-  const stream = fileHandle.createWriteStream()
+  let i = 0;
 
-  for(let i=0;i<1000000;i++){
-    const buff = Buffer.from(` ${i} `,"utf-8")
-    stream.write(buff)
-  }
+  const writeMany = () => {
+    while (i < 1000000) {
+      const buff = Buffer.from(` ${i} `, "utf-8");
 
-  console.timeEnd("writeMany")
+      // this is our last writ
+      // emit "finish" fn
+      if(i === 999999){
+        return stream.end(buff)
+      }
+
+      // if returns false, stop the loop
+      if(!stream.write(buff)){
+        break;
+      }
+      i++;
+    }
+  };
+
+  writeMany();
+
+  // resume our loop once our stream's internal buffer is empty
+  stream.on("drain",()=>{
+    writeMany();
+  })
+
+  stream.on("finish", ()=>{
+    console.timeEnd("writeMany");
+    fileHandle.close()
+  })
+
 })();
