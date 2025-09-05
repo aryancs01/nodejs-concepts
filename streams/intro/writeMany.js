25db6f9bf938dc7432adbbd3a85869cbd294cb9b
aryancs01 <aryansaxenacs@gmail.com>
2025-09-05 15:08:48 +0530

    streams code

--- streams/intro/writeMany.js
+++ streams/intro/writeMany.js
@@ -17,15 +17,37 @@
 
 // Callback fn
 // As docs says, it is more useful for expensive operation
-
-const fs = require("node:fs");
-(async () => {
-  console.time("writeMany");
-  fs.open("text.txt", "w", (err, fd) => {
-    for(let i=0;i<1000000;i++){
-        fs.writeSync(fd,"AA")
-    }
-  });
-  console.timeEnd("writeMany");
-})();
+// Execution are not in order
+// Memory ususge is high 
+// const fs = require("node:fs");
+// (async () => {
+//   console.time("writeMany");
+//   fs.open("text.txt", "w", (err, fd) => {
+//     for(let i=0;i<1000000;i++){
+//         fs.writeSync(fd,"AA")
+//     }
+//   });
+//   console.timeEnd("writeMany");
+// })();
 // And Actually, it is very fast
+
+
+
+//------------------------------------------------
+// Using Streams
+// Its way way faster!
+// Memory usuage is still very high
+const fs = require("node:fs/promises");
+(async () => {
+  console.time("writeMany")
+  const fileHandle = await fs.open("test.txt","w");
+
+  const stream = fileHandle.createWriteStream()
+
+  for(let i=0;i<1000000;i++){
+    const buff = Buffer.from(` ${i} `,"utf-8")
+    stream.write(buff)
+  }
+
+  console.timeEnd("writeMany")
+})();
